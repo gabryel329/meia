06ba88ad02ee69cd9c509639d25c15465ce32ec5
gabryel329 <gabryel329@gmail.com>
2024-05-16 16:36:03 -0300

    ADD

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -112,7 +112,7 @@
               <a class="d-flex flex-column flex-lg-row gap-3 align-items-start align-items-lg-center py-3 link-body-emphasis text-decoration-none border-top" href="#">
                 <svg class="bd-placeholder-img" width="100%" height="96" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
                 <div class="col-lg-8">
-                  <h6 class="mb-0">Melhores Jogadores</h6>
+                  <h6 class="mb-0">Melhores Goleiros</h6>
                 </div>
               </a>
             </li>
@@ -120,7 +120,7 @@
               <a class="d-flex flex-column flex-lg-row gap-3 align-items-start align-items-lg-center py-3 link-body-emphasis text-decoration-none border-top" href="#">
                 <svg class="bd-placeholder-img" width="100%" height="96" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
                 <div class="col-lg-8">
-                  <h6 class="mb-0">Tabela Brasileirão</h6>
+                  <h6 class="mb-0">Melhores Atacantes</h6>
                 </div>
               </a>
             </li>
